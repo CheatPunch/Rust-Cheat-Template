fe146d64cdd811ba557c4acffbab86e01929c3ea
CheatPunch <37700973+CheatPunch@users.noreply.github.com>
2018-03-23 17:19:39 +0300

    Add files via upload

---
+++ CheatTemplate/Loader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CheatTemplate
+{
+    public class Loader
+    {
+        public static void Load()
+        {
+            GameObject menuGameObject = new GameObject("menuObj");
+            menuGameObject.AddComponent<Menu>();
+            Object.DontDestroyOnLoad(menuGameObject);
+        }
+    }
+}
---
+++ CheatTemplate/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace CheatTemplate
+{
+    public class Menu : MonoBehaviour
+    {
+        private bool _toggled;
+        private readonly GUIStyle _myStyle = new GUIStyle();
+
+        private void Start()
+        {
+            _myStyle.fontSize = 21;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                _toggled = !_toggled;
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (_toggled)
+            {
+                GUI.Label(new Rect(Screen.width / 2f, Screen.height / 2f, 150f, 80f), "Rust Cheat Template - https://github.com/CheatPunch");
+            }
+        }
+    }
+}
